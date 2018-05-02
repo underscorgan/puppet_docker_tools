58d6259c57766e08d053e871c7384b8a5f4bea09
Morgan Rhodes <morgan@puppet.com>
2018-05-02 11:15:08 -0700

    (maint) build utils

--- lib/puppet_docker_tools.rb
+++ lib/puppet_docker_tools.rb
@@ -4,4 +4,8 @@ $:.unshift(LIBDIR) unless
 $:.include?(File.dirname(__FILE__)) || $:.include?(LIBDIR)
 
 class PuppetDockerTools
+  REPOSITORY = ENV['DOCKER_REPOSITORY'] || 'puppet'
+  NO_CACHE = ENV['DOCKER_NO_CACHE'] || false
+  TAG = ENV['DOCKER_IMAGE_TAG'] || 'latest'
+  NAMESPACE = ENV['DOCKER_NAMESPACE'] || 'org.label-schema'
 end
