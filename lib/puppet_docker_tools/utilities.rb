894162af769e96f5ee9f6f4c289d52a4257938d2
Morgan Rhodes <morgan@puppet.com>
2018-05-02 17:17:38 -0700

    (maint) Add auth capabilities and push subcommand

--- lib/puppet_docker_tools/utilities.rb
+++ lib/puppet_docker_tools/utilities.rb
@@ -1,5 +1,33 @@
 require 'puppet_docker_tools/utilities/dockerfile'
+require 'docker'
+
 class PuppetDockerTools
   module Utilities
+    module_function
+
+    def authenticate
+      puts "Authentication for hub.docker.com"
+      print "Username: "
+      STDOUT.flush
+      username = gets.chomp
+      print "Password: "
+      STDOUT.flush
+      password = STDIN.noecho(&:gets).chomp
+      puts
+      print "Email: "
+      STDOUT.flush
+      email = gets.chomp
+
+      puts "going to auth to hub.docker.com as #{username} with #{email}"
+
+      Docker.authenticate!('username' => username, 'password' => password, 'email' => email)
+    end
+
+    def get_value_from_label(image, value)
+      labels = Docker::Image.get("#{PuppetDockerTools::REPOSITORY}/#{image}").json["Config"]["Labels"]
+      labels["#{PuppetDockerTools::NAMESPACE}.#{value.tr('_', '-')}"]
+    rescue
+      nil
+    end
   end
 end
