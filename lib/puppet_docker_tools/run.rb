425e60337fa71f63b8618e07eae6dfcdaaad4236
Morgan Rhodes <morgan@puppet.com>
2018-05-11 10:49:24 -0700

    (maint) Add docs for PuppetDockerTools::Run

--- lib/puppet_docker_tools/run.rb
+++ lib/puppet_docker_tools/run.rb
@@ -9,12 +9,21 @@ class PuppetDockerTools
   module Run
     module_function
 
+    # Build a docker image from a directory
+    #
+    # @param directory The directory containing the Dockerfile you're building
+    #        the image from
+    # @param repository The repository this image will be pushed to
+    # @param namespace The namespace for the version label in the dockerfile
+    # @param no_cache Whether or not to use existing layer caches when building
+    #        this image. Defaults to using the cache (no_cache = false).
     def build(directory, repository: , namespace: ,no_cache: false)
       image_name = File.basename(directory)
-
       version = PuppetDockerTools::Utilities.get_value_from_env('version', namespace: namespace, directory: directory)
       path = "#{repository}/#{image_name}"
       puts "Building #{path}:latest"
+
+      # 't' in the build_options sets the tag for the image we're building
       build_options = { 't' => "#{path}:latest" }
       if no_cache
         puts "Ignoring cache for #{path}"
@@ -24,16 +33,24 @@ class PuppetDockerTools
 
       if version
         puts "Building #{path}:#{version}"
+
+        # 't' in the build_options sets the tag for the image we're building
         build_options = { 't' => "#{path}:#{version}" }
         Docker::Image.build_from_dir(directory, build_options)
       end
     end
 
+    # Run hadolint on the Dockerfile in the specified directory
+    #
+    # @param directory
     def lint(directory)
       hadolint_container = 'lukasmartinelli/hadolint'
+      # make sure we have the container locally
       PuppetDockerTools::Run.pull("#{hadolint_container}:latest")
       container = Docker::Container.create('Cmd' => ['/bin/sh', '-c', "hadolint --ignore DL3008 --ignore DL4000 --ignore DL4001 - "], 'Image' => hadolint_container, 'OpenStdin' => true, 'StdinOnce' => true)
+      # This container.tap startes the container created above, and passes directory/Dockerfile to the container
       container.tap(&:start).attach(stdin: "#{directory}/Dockerfile")
+      # Wait for the run to finish
       container.wait
       exit_status = container.json['State']['ExitCode']
       unless exit_status == 0
@@ -42,6 +59,10 @@ class PuppetDockerTools
       end
     end
 
+    # Pull a docker image
+    #
+    # @param image The image to pull. If the image does not include the tag to
+    #        pull, it will pull all tags for that image
     def pull(image)
       if image.include?(':')
         puts "Pulling #{image}"
@@ -52,9 +73,13 @@ class PuppetDockerTools
       end
     end
 
+    # Pull all tags for a docker image
+    #
+    # @param image The image to pull, e.g. puppet/puppetserver
     def pull_all_tags(image)
       Docker::Image.create('fromImage' => image)
 
+      # Filter through existing tags of that image so we can output what we pulled
       images = Docker::Image.all('filter' => image)
       images.each do |img|
         timestamp = PuppetDockerTools::Utilities.format_timestamp(img.info["Created"])
@@ -62,19 +87,27 @@ class PuppetDockerTools
       end
     end
 
+    # Pull a single tag of a docker image
+    #
+    # @param tag The image/tag to pull, e.g. puppet/puppetserver:latest
     def pull_single_tag(tag)
       image = Docker::Image.create('fromImage' => tag)
       timestamp = PuppetDockerTools::Utilities.format_timestamp(image.info["Created"])
       puts "Pulled #{image.info["RepoTags"].first}, last updated #{timestamp}"
     end
 
+    # Push an image to hub.docker.com
+    #
+    # @param directory The directory containing the Dockerfile for the image you
+    #        want to push
+    # @param repository The repository this will be pushed to
+    # @param namespace The namespace for the version label in the dockerfile
     def push(directory, repository: , namespace: )
       image_name = File.basename(directory)
-
       path = "#{repository}/#{image_name}"
-
       version = PuppetDockerTools::Utilities.get_value_from_label(path, value: 'version', namespace: namespace)
 
+      # We always want to push a versioned label in addition to the latest label
       unless version
         fail "No version specified in Dockerfile for #{path}"
       end
@@ -94,7 +127,10 @@ class PuppetDockerTools
       end
     end
 
-
+    # Update vcs-ref and build-date labels in the Dockerfile
+    #
+    # @param directory The directory containing the Dockerfile to update
+    # @param namespace The namespace for the labels
     def rev_labels(directory, namespace: )
       dockerfile = File.join(directory, 'Dockerfile')
 
@@ -117,6 +153,10 @@ class PuppetDockerTools
       File.open(dockerfile, 'w') { |file| file.puts text }
     end
 
+    # Run spec tests
+    #
+    # @param directory The directory containing image build info. This command
+    #        looks for spec tests under "#{directory}/spec/*_spec.rb
     def spec(directory)
       tests = Dir.glob("#{directory}/spec/*_spec.rb")
       test_files = tests.map { |test| File.basename(test, '.rb') }
@@ -125,12 +165,19 @@ class PuppetDockerTools
       RSpec::Core::Runner.run(tests, $stderr, $stdout)
     end
 
+    # Pull the specified tags
+    #
+    # @param tags [Array] A list of tags to pull, e.g. ['centos:7', 'ubuntu:16.04']
     def update_base_images(tags)
       tags.each do |tag|
         PuppetDockerTools::Run.pull(tag)
       end
     end
 
+    # Get the version set in the Dockerfile in the specified directory
+    #
+    # @param directory
+    # @param namespace The namespace for the version label
     def version(directory, namespace: )
       puts PuppetDockerTools::Utilities.get_value_from_env('version', namespace: namespace, directory: directory)
     end
