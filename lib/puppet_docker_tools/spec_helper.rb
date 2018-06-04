2c118d849c420642adb056e08e11a3bceaff8b2f
Morgan Rhodes <morgan@puppet.com>
2018-06-04 15:27:27 -0700

    (maint) Up the timeout

--- lib/puppet_docker_tools/spec_helper.rb
+++ lib/puppet_docker_tools/spec_helper.rb
@@ -2,7 +2,7 @@ require 'serverspec'
 require 'docker'
 
 # Travis builds can take time
-Docker.options[:read_timeout] = 7200
+Docker.options[:read_timeout] = 36000
 
 # Load any shared examples or context helpers
 Dir[File.join(File.dirname(__FILE__), '..', '..', 'spec', 'support', '**', '*.rb')].sort.each { |f| require f }
