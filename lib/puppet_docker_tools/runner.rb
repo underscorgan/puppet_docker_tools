2f452ef9406662179260d2612e685a5a3354d429
Morgan Rhodes <morgan@puppet.com>
2018-06-08 10:11:24 -0700

    (maint) Pass buildargs for vcs_ref and build_date to image build step

--- lib/puppet_docker_tools/runner.rb
+++ lib/puppet_docker_tools/runner.rb
@@ -1,5 +1,6 @@
 require 'date'
 require 'docker'
+require 'json'
 require 'rspec/core'
 require 'time'
 require 'puppet_docker_tools/utilities'
@@ -29,8 +30,14 @@ class PuppetDockerTools
       path = "#{repository}/#{image_name}"
       puts "Building #{path}:latest"
 
+      build_args = {
+        'vcs_ref' => PuppetDockerTools::Utilities.current_git_sha(directory),
+        'build_date' => Time.now.utc.iso8601
+      }
+
       # 't' in the build_options sets the tag for the image we're building
-      build_options = { 't' => "#{path}:latest", 'dockerfile' => dockerfile }
+      build_options = { 't' => "#{path}:latest", 'dockerfile' => dockerfile, 'buildargs' => "#{build_args.to_json}"}
+
       if no_cache
         puts "Ignoring cache for #{path}"
         build_options['nocache'] = true
