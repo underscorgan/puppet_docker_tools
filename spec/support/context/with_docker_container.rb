d2179ffc9c51754cb1c254c5dc7fa19f872ce6f8
Morgan Rhodes <morgan@puppet.com>
2018-09-13 11:10:54 -0700

    (maint) Make sure container is running before we start tests

--- spec/support/context/with_docker_container.rb
+++ spec/support/context/with_docker_container.rb
@@ -1,6 +1,38 @@
+require 'open3'
+
 shared_context 'with a docker container' do
+  def is_running?(container)
+    is_running = false
+    command = ['docker', 'inspect', '-f', "'{{.State.Health.Status}}'", "#{container}"]
+    while ! is_running
+      output, status = Open3.capture2e(*command)
+      output.chomp!
+      output.gsub!(/'/, '')
+
+      # If finding State.Health.Status throws an error, we probably don't have
+      # a healthcheck, so let's fall back to State.Running
+      if status.exitstatus != 0
+        command = ['docker', 'inspect', '-f', "'{{.State.Running}}'", "#{container}"]
+      end
+
+      if output == "healthy"
+        return true
+      elsif output == "true"
+        return true
+      elsif output == "unhealthy"
+         return false
+      end
+
+      puts "Container is not running yet, will try again in 5 seconds..."
+      sleep(5)
+    end
+  end
+
   before(:all) do
     @container = %x(docker run --detach --rm -i #{@image}).chomp
+    unless is_running?(@container)
+      fail "something went wrong with container startup!"
+    end
   end
 
   after(:all) do
